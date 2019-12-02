643977fa8ae93dbf0efa641924f625dc69047acb
Aditya Ekawade <aditya.ekawade@gmail.com>
2019-12-02 16:42:12 -0700

    Show search status of gtr

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,18 @@ import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
+// export default new Vuetify({
+// });
+
 export default new Vuetify({
-});
+  theme: {
+    themes: {
+      light: {
+        primary: '#45688e',
+      },
+      dark: {
+        primary: '#45688e',
+      },
+    },
+  },
+})
