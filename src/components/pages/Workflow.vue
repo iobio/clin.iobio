aebbdd16c1be63ac23c9025e9cd18b97ce98e593
Tonya Di Sera <tony.disera@gmail.com>
2020-01-28 18:40:37 -0700

    More work on styling workflow card

--- src/components/pages/Workflow.vue
+++ src/components/pages/Workflow.vue
@@ -10,11 +10,11 @@ $badge-inactive-color:  #d8d3d3
   // height: 140px
   // margin-top: 50px
   height: auto
-  margin-bottom: 10px
-  padding-top: 10px
-  padding-bottom: 0px
+  margin-bottom: 8px
   padding-right: 10px
   padding-left: 10px
+  padding-top: 5px
+  padding-bottom: 10px
   text-align: center
 
 
@@ -118,8 +118,8 @@ $badge-inactive-color:  #d8d3d3
       font-size: 12px
       font-weight: 500
       font-style: italic
-      right: -65px
-      top: 1px
+      right: -70px
+      top: 6px
       color: $current-color
       background-color: transparent
       line-height: 13px
@@ -245,10 +245,10 @@ $badge-inactive-color:  #d8d3d3
       height: 2px
 
       &.short
-        width: 60px
+        width: 70px
 
       &.long
-        width: 100px
+        width: 80px
 
       &.invisible
         visibility: hidden
@@ -262,6 +262,7 @@ $badge-inactive-color:  #d8d3d3
       font-family: Raleway
       font-size: 15px
       text-align: center
+      margin-left: 100px
 
 
       &.first
@@ -299,12 +300,12 @@ $badge-inactive-color:  #d8d3d3
             color: white !important
 
       .task-label
-        width:       120px
+        width:       140px
         font-size:   13px
         line-height: 14px
         text-align:  center
-        min-height:  66px
-        max-height:  66px
+        min-height:  55px
+        max-height:  55px
         font-weight: 500
 
       .avatar-button
@@ -345,6 +346,7 @@ $badge-inactive-color:  #d8d3d3
   #current-checkbox-container
     margin-top: -52px
     max-width: 30px
+    height: 35px
 
     label
       display: none
@@ -356,8 +358,11 @@ $badge-inactive-color:  #d8d3d3
     padding-top: 3px
 
     &.active
-      background-color: #f6f6f6
-      border: solid thin #dbd4d4
+
+      .step-label
+        background-color: #f6f6f6
+        border: solid .5px #d1d1d1
+
       .task
         &.active
           .avatar-button
@@ -544,7 +549,7 @@ export default {
 
           let offsetLeft = $('#workflow-steps')[0].offsetLeft;
 
-          let offset = taskIdx == 0 ? 58 : 58;
+          let offset = taskIdx == 0 ? 67 : 67;
           self.currentTaskLeft = $('.task.active')[0].offsetLeft - offsetLeft + offset + 'px';
 
           if (taskIdx == self.currentStep.tasks.length - 1 && stepIdx == self.analysisSteps.length - 1) {
