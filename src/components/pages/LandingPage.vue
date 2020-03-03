887898a017d538dc9723214c5cc435cae6381c35
Aditya Ekawade <aditya.ekawade@gmail.com>
2020-03-03 10:59:10 -0800

    Add workflow steps list items- landing page

--- src/components/pages/LandingPage.vue
+++ src/components/pages/LandingPage.vue
@@ -60,98 +60,147 @@
             </v-responsive>
           <!-- </v-card> -->
         </v-flex>
+      </v-layout>
+      
+      
+      <v-layout row wrap style="background:white">
+        <v-container>
+          <v-layout row wrap>
+            <v-flex xs2>
+              <!-- Workflow steps 
+              <p>
+                Review case 
+                <br><br>
+              </p>  -->
+              <v-list rounded>
+                <v-subheader>Workflow steps</v-subheader>
+                <v-divider></v-divider>
+                <v-list-item-group color="primary">
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title >Review case</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title >Review phenotypes</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title >Review variants</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title >Findings</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                </v-list-item-group>
+              </v-list>
+            </v-flex>
+            <v-flex xs10>
+              <hooper :vertical="true" style="height: 400px; background:white" :itemsToShow="1" :centerMode="true" :transition="1050">
+                <hooper-navigation slot="hooper-addons"></hooper-navigation>
+                <hooper-progress slot="hooper-addons"></hooper-progress>
+                <slide>
+                  <v-container fluid>
+                    <v-layout row wrap>
+                      <v-flex xs6>
+                        <img style="width:700px;" src="./review_case.png" alt="review_case">
+                      </v-flex>
+                      <v-flex xs1></v-flex>
+                      <v-flex xs3 class="mt-12">
+                        <span class="i-hooper_text">Review case</span>
+                        <br><br>
+                        <span class="i-hooper_subheading">
+                          Review relatedness, case description, and
+                          data quality for your data
+                        </span>
+                      </v-flex>
+                      <v-flex xs2></v-flex>
+                    </v-layout>
+                  </v-container>
+                </slide>
+                <slide >
+                  <v-container fluid>
+                    <v-layout row wrap>
+                      <v-flex xs6>
+                        <img style="width:700px;" src="./review_phenotypes.png" alt="review_phenotypes">
+                      </v-flex>
+                      <v-flex xs1></v-flex>
+                      <v-flex xs3 class="mt-12">
+                        <span class="i-hooper_text">Review phenotypes</span>
+                        <br><br>
+                        <span class="i-hooper_subheading">
+                          Review phenotypes from entered 
+                          clinical notes and generate a prioritized 
+                          gene list
+                        </span>
+                      </v-flex>
+                      <v-flex xs2></v-flex>
+
+                    </v-layout>
+                  </v-container>
+
+                </slide>
+                <slide>
+                  <v-container fluid>
+                    <v-layout row wrap>
+                      <v-flex xs6>
+                        <img style="width:700px;" src="./review_variants.png" alt="review_variants">
+                      </v-flex>
+                      <v-flex xs1></v-flex>
+                      <v-flex xs3 class="mt-12">
+                        <span class="i-hooper_text">Review variants</span>
+                        <br><br>
+                        <span class="i-hooper_subheading">
+                          Review and mark variants as significant, 
+                          Variants of unknown significance and 
+                          Add notes
+                        </span>
+                      </v-flex>
+                      <v-flex xs2></v-flex>
+
+                    </v-layout>
+                  </v-container>
+
+                </slide>
+
+                <slide>
+                  <v-container fluid>
+                    <v-layout row wrap>
+                      <v-flex xs6>
+                        <img style="width:700px;" src="./findings.png" alt="Findings">
+                      </v-flex>
+                      <v-flex xs1></v-flex>
+                      <v-flex xs3 class="mt-12">
+                        <span class="i-hooper_text">Findings</span>
+                        <br><br>
+                        <span class="i-hooper_subheading">
+                          Review results from all stages of the 
+                          workflow and download report
+                        </span>
+                      </v-flex>
+                      <v-flex xs2></v-flex>
+
+                    </v-layout>
+                  </v-container>
+
+                </slide>
+              </hooper>
+
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-layout>
+      
+      
+      <v-layout row wrap>
         
-        <hooper style="height: 400px; background:white" :itemsToShow="1" :centerMode="true" :transition="1150">
-          <hooper-navigation slot="hooper-addons"></hooper-navigation>
-          <hooper-progress slot="hooper-addons"></hooper-progress>
-          <slide>
-            <v-container fluid>
-              <v-layout row wrap>
-                <v-flex xs1></v-flex>
-                <v-flex xs6>
-                  <img style="width:700px;" src="./review_case.png" alt="review_case">
-                </v-flex>
-                <v-flex xs3 class="mt-12">
-                  <span class="i-hooper_text">Review case</span>
-                  <br><br>
-                  <span class="i-hooper_subheading">
-                    Review relatedness, case description, and
-                    data quality for your data
-                  </span>
-                </v-flex>
-                <v-flex xs2></v-flex>
-              </v-layout>
-            </v-container>
-          </slide>
-          <slide >
-            <v-container fluid>
-              <v-layout row wrap>
-                <v-flex xs1></v-flex>
-                <v-flex xs6>
-                  <img style="width:700px;" src="./review_phenotypes.png" alt="review_phenotypes">
-                </v-flex>
-                <v-flex xs3 class="mt-12">
-                  <span class="i-hooper_text">Review phenotypes</span>
-                  <br><br>
-                  <span class="i-hooper_subheading">
-                    Review phenotypes from entered 
-                    clinical notes and generate a prioritized 
-                    gene list
-                  </span>
-                </v-flex>
-                <v-flex xs2></v-flex>
-
-              </v-layout>
-            </v-container>
-
-          </slide>
-          <slide>
-            <v-container fluid>
-              <v-layout row wrap>
-                <v-flex xs1></v-flex>
-                <v-flex xs6>
-                  <img style="width:700px;" src="./review_variants.png" alt="review_variants">
-                </v-flex>
-                <v-flex xs3 class="mt-12">
-                  <span class="i-hooper_text">Review variants</span>
-                  <br><br>
-                  <span class="i-hooper_subheading">
-                    Review and mark variants as significant, 
-                    Variants of unknown significance and 
-                    Add notes
-                  </span>
-                </v-flex>
-                <v-flex xs2></v-flex>
-
-              </v-layout>
-            </v-container>
-
-          </slide>
-
-          <slide>
-            <v-container fluid>
-              <v-layout row wrap>
-                <v-flex xs6>
-                  <img style="width:700px;" src="./findings.png" alt="Findings">
-                </v-flex>
-                <v-flex xs3 class="mt-12">
-                  <span class="i-hooper_text">Findings</span>
-                  <br><br>
-                  <span class="i-hooper_subheading">
-                    Review results from all stages of the 
-                    workflow and download report
-                  </span>
-                </v-flex>
-                <v-flex xs2></v-flex>
-
-              </v-layout>
-            </v-container>
-
-          </slide>
-        </hooper>
-        
-        
-
       </v-layout>
     </v-content>
 
