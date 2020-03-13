117e4a2dc6c75ea8c3483b3855e6649b33172f40
Aditya Ekawade <aditya.ekawade@gmail.com>
2020-03-13 11:42:48 -0700

    Add deploy to stage

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,11 @@ if [[ $1 == "prod" ]]; then
 	aws s3 cp ./dist/  s3://static.iobio.io/prod/clin.iobio.io/ --recursive
 	echo "** Renew cloudfrount cache **"
 	aws cloudfront create-invalidation --distribution-id E2Y2NGXFSUAHUW --paths /\*
+elif [[ $1 == "stage" ]]; then
+	echo "** Syncing to stage s3 bucket **"
+	aws s3 cp ./dist/  s3://static.iobio.io/stage/clin.iobio.io/ --recursive
+	echo "** Renew cloudfrount cache **"
+	aws cloudfront create-invalidation --distribution-id EQCQ1T6VJWZTJ --paths /\*
 else
 	echo "** Syncing to dev s3 bucket **"
 	aws s3 cp ./dist/  s3://static.iobio.io/dev/clin.iobio.io/ --recursive
