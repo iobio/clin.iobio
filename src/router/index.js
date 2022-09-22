82b77a2d4ec4026ec91fffca1b3c1480a61a772c
TONYA DI SERA <tony.disera@gmail.com>
2022-09-22 11:57:29 -0600

    Launch stand-alone clin.iobio with URL parameters for variant and alignment files (like gene.iobio) #484

--- src/router/index.js
+++ src/router/index.js
@@ -43,7 +43,21 @@ const routes = [
 
         paramTheme:                 route.query.theme,
         paramVariantSetId:          route.query.variant_set_id,
-        paramExperimentId:          route.query.experiment_id
+        paramExperimentId:          route.query.experiment_id,
+
+        paramAffectedSibs:     route.query.affectedSibs,
+        paramUnaffectedSibs:   route.query.unaffectedSibs,
+        paramRelationships:    [route.query.rel0, route.query.rel1, route.query.rel2],
+        paramSexes:            [route.query.sex0, route.query.sex1, route.query.sex2],
+        paramSamples:          [route.query.sample0, route.query.sample1, route.query.sample2],
+        paramNames:            [route.query.name0, route.query.name1, route.query.name2],
+        paramBams:             [route.query.bam0, route.query.bam1, route.query.bam2],
+        paramBais:             [route.query.bai0, route.query.bai1, route.query.bai2],
+        paramVcfs:             [route.query.vcf0, route.query.vcf1, route.query.vcf2],
+        paramTbis:             [route.query.tbi0, route.query.tbi1, route.query.tbi2],
+        paramAffectedStatuses: [route.query.affectedStatus0, route.query.affectedStatus1, route.query.affectedStatus2],
+        
+
     })
   },
    {
